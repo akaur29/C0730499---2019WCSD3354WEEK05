73ddf0c9c8e638552f61c408fee667202c47d991
visualstudio <visualstudio@230-PC43>
2019-02-07 14:27:27 -0500

    2:05

--- week5/Program.cs
+++ week5/Program.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace week5
+namespace ConsoleApp3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var a = new TestQuestion2();
+            a.PlayingWithForLoops();
+        }
+        class TestQuestion2
+        {
+            public void PlayingWithForLoops()
+            {
+                for (; mymethod();)
+                {
+                    if (myfavouritevariables > 10)
+                    {
+                        Console.WriteLine("i am in for loops");
+                        break;
+                    }
+                }
+                Console.WriteLine("oh no i have to go through this stupoid loop again");
+            }
+        }
+        public bool mymethod()
+        {
+            myfavouritevariables
+                return true;
         }
     }
+}
 }
