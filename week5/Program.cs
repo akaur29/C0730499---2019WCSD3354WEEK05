94fcd1bc9d153bacb61a9a5b9b2c14e134779a2e
visualstudio <visualstudio@230-PC43>
2019-02-07 16:24:55 -0500

    77

--- week5/Program.cs
+++ week5/Program.cs
@@ -9,6 +9,8 @@ namespace ConsoleApp3
 {
     class Program
     {
+        private readonly object myfavouritevariables;
+
         static void Main(string[] args)
         {
             var a = new TestQuestion2();
@@ -54,13 +56,47 @@ namespace ConsoleApp3
     }
     class birthday_party
         {
-        public dog fifo;
+        public dog fifi;
         public dog peanut;
         public dog clarence;
         public dog roy;
         public dog head;
 
+        public dog temporary { get; private set; }
+
+        public void setupPartyList()
+        {
+            peanut = new dog("peanut", "bichon");
+            fifi = new dog("fifi", "poodle");
+            clarence = new dog("clarance", "german sheppard");
+            roy = new dog("roy", "beagle");
+
+            peanut.previous_dog = null;
+            peanut.next_dog = null;
+            fifi.previous_dog = peanut;
+            fifi.next_dog = clarence;
+            clarence.previous_dog = fifi;
+            clarence.next_dog = roy;
+            head.previous_dog = roy;
+            head.next_dog = null;
+
         }
+        public string printPartyList()
+
+        {
+            string inviteList = "*---";
+            temporary = head;
+            while(temporary.next_dog!=null)
+            {
+                inviteList += temporary.dog_name + "*----*";
+            }
+        }
+        return inviteList;
+private class inviteList
+        {
+        }
+    }
+    
     
 }
 }
